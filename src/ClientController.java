f9252bb37e65d906e3c6870949bb6a64dbd9926a
blackenedgem <Chrisroberts97@btinternet.com>
2018-02-28 14:21:45 +0000

    Integrate FXML and java code UI

--- src/ClientController.java
+++ src/ClientController.java
@@ -1,2 +1,29 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
 public class ClientController {
+    private static String DEFAULT_IP = "localhost";
+    private static int DEFAULT_PORT = 1100;
+
+    // Connection UI
+    @FXML private TextField textIP;
+    @FXML private TextField textPort;
+    @FXML private Button connect;
+    @FXML private Button quit;
+
+    // Operations
+    @FXML private Button delf;
+    @FXML private Button dwld;
+    @FXML private Button list;
+    @FXML private Button upld;
+
+    // Listview
+    @FXML private ListView<String> listView;
+
+    @FXML
+    public void initialize() {
+
+    }
 }
