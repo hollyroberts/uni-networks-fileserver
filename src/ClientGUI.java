3ba89265f0ddb496d109119bf561083d31efac3a
blackenedgem <Chrisroberts97@btinternet.com>
2018-02-28 14:15:26 +0000

    Tweak GUI, add java launcher

---
+++ src/ClientGUI.java
@@ -0,0 +1,22 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class ClientGUI extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("client.fxml"));
+        stage.setTitle("Client");
+        stage.setScene(new Scene(root, 600, 400));
+        stage.setMinWidth(400);
+        stage.setMinHeight(300);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Launching GUI");
+        launch(args);
+    }
+}
