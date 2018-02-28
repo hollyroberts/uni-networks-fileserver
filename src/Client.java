08ae31930d9f7f43fcdbf5e5fee73858b6954764
blackenedgem <Chrisroberts97@btinternet.com>
2018-02-28 17:12:38 +0000

    Client functionality of list

--- src/Client.java
+++ src/Client.java
@@ -17,6 +17,33 @@ class Client {
         this.out = output;
     }
 
+    public void list() throws IOException, InterruptedException {
+        Log.log("Retrieving listings");
+        out.writeUTF("LIST");
+
+        // Number of listings to retrieve
+        Misc.waitForInput(in, 1);
+        int numListings = in.readInt();
+        if (numListings <= 0) {
+            Log.log("Server contains no listings");
+            return;
+        }
+
+        // Retrieve listings
+        String[] listings = new String[numListings];
+        for (int i = 0; i < numListings; i++) {
+            Misc.waitForInput(in, 1);
+            listings[i] = in.readUTF();
+        }
+
+        // Once listings are retrieved, display to client
+        Log.log("Listings:");
+        for (String listing : listings) {
+            Log.log(listing);
+        }
+    }
+
+    // Attempts to quit gracefully using operations
     public void quit() {
         try {
             out.writeUTF("QUIT");
@@ -53,7 +80,7 @@ class Client {
 
     // The code that performs the upload (wrapped in upload to handle errors)
     private void uploadLogic(String filename, byte[] bytes) throws IOException, InterruptedException {
-        Log.log("Sending UPLD operation to server");
+        Log.log("Sending UPLD operation to server and waiting for response");
         out.writeUTF("UPLD");
         out.writeShort(filename.length());
         out.writeChars(filename);
