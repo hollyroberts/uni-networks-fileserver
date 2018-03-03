49880554577ad26b8aaf3f5f5b7553e516a230db
blackenedgem <Chrisroberts97@btinternet.com>
2018-03-03 15:27:41 +0000

    Allow server timeout to be configured

--- src/Server.java
+++ src/Server.java
@@ -2,11 +2,12 @@ import java.io.File;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Server {
     private static int PORT_NUMBER = 1234;
-    public static int TIMEOUT = 5000;
     public static String BASE_DIR = "server_files/";
+    public static int TIMEOUT = 5000;
 
     private void run() throws Exception {
         ServerSocket serverSocket = new ServerSocket(PORT_NUMBER);
@@ -38,6 +39,17 @@ public class Server {
             }
         }
 
+        // Enter timeout
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter socket timeout in ms (Leave empty for default): ");
+        String timeoutStr = scanner.nextLine();
+
+        try {
+            TIMEOUT = Integer.valueOf(timeoutStr);
+            System.out.println(String.format("Set timeout to %,dms", TIMEOUT));
+        } catch (NumberFormatException e) {
+            System.out.println(String.format("Timeout set to default value of %,dms", TIMEOUT));
+        }
 
         new Server().run();
     }
